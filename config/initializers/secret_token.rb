a94112ea94b66f10f5236ab885f56fd7fd74c19a
sakoh <sakohartounian@yahoo.com>
2013-10-16 21:27:54 -0700

    .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Clonepress::Application.config.secret_key_base = 'adb4100a6c3643189a30fa6e7d64a1651c17b4aa8ac8672f586b062939ac6fabb34c8cbce84a178bd852cd16a30ce2e1c534d648a7e916b2b5f793a6e2e845c9'
